3706c662b0f260a2dc1a7bc350a76225ccc90414
amarderschrecklicher <atahirovic3@etf.unsa.ba>
2025-03-28 11:35:12 +0100

    eureka configured

--- eureka/src/main/java/ba/etf/unsa/eureka/eureka/EurekaApplication.java
+++ eureka/src/main/java/ba/etf/unsa/eureka/eureka/EurekaApplication.java
@@ -2,8 +2,13 @@ package ba.etf.unsa.eureka.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
