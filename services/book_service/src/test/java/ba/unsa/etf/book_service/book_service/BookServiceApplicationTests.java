069a6c6d4d181a83e82c9c3769c76172420a1fa9
amarderschrecklicher <atahirovic3@etf.unsa.ba>
2025-04-09 22:13:03 +0200

    tests

--- services/book_service/src/test/java/ba/unsa/etf/book_service/book_service/BookServiceApplicationTests.java
+++ services/book_service/src/test/java/ba/unsa/etf/book_service/book_service/BookServiceApplicationTests.java
@@ -5,12 +5,10 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.*;
 import static org.assertj.core.api.Assertions.assertThat;
