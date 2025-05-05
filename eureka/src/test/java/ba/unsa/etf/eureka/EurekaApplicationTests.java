5618232e4498798e880315e4115b62a83348f143
amarderschrecklicher <atahirovic3@etf.unsa.ba>
2025-05-05 06:17:55 +0200

    centralized configuration

--- eureka/src/test/java/ba/unsa/etf/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/ba/unsa/etf/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package ba.etf.unsa.eureka.eureka;
+package ba.unsa.etf.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EurekaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
